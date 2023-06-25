8efa1f81706111112430fd8d43f2684d85090c19
M-Nikhil-guptha <101795825+M-Nikhil-guptha@users.noreply.github.com>
2023-06-25 23:13:27 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "d0dc08308f2fee0f6690e494b96a8ff6",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
